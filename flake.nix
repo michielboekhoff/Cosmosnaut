78103ad1a37739c283113c0b710e65cdaec39516
Michiel Boekhoff <michielalexanderb@gmail.com>
2024-02-09 14:14:46 +0000

    Add rust-analyzer to flake.nix

--- flake.nix
+++ flake.nix
@@ -133,6 +133,7 @@
           # Extra inputs can be added here; cargo and rustc are provided by default.
           packages = [
             # pkgs.ripgrep
+						pkgs.rust-analyzer
           ];
         };
       });
