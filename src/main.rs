ed022e92ed58c51e58feeade54f8e83cd892bc18
Michiel Boekhoff <michielalexanderb@gmail.com>
2024-02-16 03:53:42 +0000

    Add HashMap-backed store for Database

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,78 @@
-use anyhow::Result;
-use axum::{
-    async_trait,
-    extract::{FromRef, State},
-    http::StatusCode,
-    response::{IntoResponse, Response},
-    routing::post,
-    Json, Router,
-};
+use std::{collections::HashMap, sync::Arc};
+
+use axum::{async_trait, extract::State, http::StatusCode, routing::post, Json, Router};
 use serde::Deserialize;
+use thiserror::Error;
+use tokio::sync::Mutex;
+
+#[derive(Error, Debug)]
+pub enum DatabaseError {
+    #[error("database with id {0} already exists")]
+    DatabaseExists(String),
+}
 
 #[async_trait]
 pub trait Store {
-    async fn create_database(self, id: String) -> Result<()>;
+    async fn create_database<'a>(&'a mut self, id: String) -> Result<(), DatabaseError>;
 }
 
-#[derive(Clone)]
-struct NoopStore {}
+struct Database {
+    pub id: String,
+}
+
+struct HashMapStore {
+    entries: HashMap<String, Database>,
+}
+
+impl HashMapStore {
+    fn new() -> Self {
+        HashMapStore {
+            entries: HashMap::new(),
+        }
+    }
+}
 
 #[async_trait]
-impl Store for NoopStore {
-    async fn create_database(self, _id: String) -> Result<()> {
-        Ok(())
+impl Store for HashMapStore {
+    async fn create_database<'a>(&'a mut self, id: String) -> Result<(), DatabaseError> {
+        if self.entries.contains_key(&id) {
+            Err(DatabaseError::DatabaseExists(id))
+        } else {
+            self.entries.insert(id.clone(), Database { id: id.clone() });
+            Ok(())
+        }
     }
 }
 
-impl FromRef<AppState<Self>> for NoopStore {
-    fn from_ref(input: &AppState<Self>) -> Self {
-        input.database_store.clone()
-    }
-}
-
-#[derive(Clone)]
 struct AppState<S: Store> {
-    database_store: S,
+    database_store: Arc<Mutex<S>>,
+}
+
+impl<S: Store> AppState<S> {
+    pub fn new(store: S) -> AppState<S> {
+        AppState {
+            database_store: Arc::new(Mutex::new(store)),
+        }
+    }
+}
+
+impl<S: Store> Clone for AppState<S> {
+    fn clone(&self) -> Self {
+        Self {
+            database_store: self.database_store.clone(),
+        }
+    }
 }
 
 async fn create_database<T: Store>(
-    State(_svc): State<T>,
-    Json(_payload): Json<CreateDatabaseRequest>,
-) -> Result<Response, StatusCode> {
-    Ok(().into_response())
+    State(svc): State<AppState<T>>,
+    Json(payload): Json<CreateDatabaseRequest>,
+) -> StatusCode {
+    let mut x = svc.database_store.lock().await;
+    match x.create_database(payload.id).await {
+        Ok(_) => StatusCode::OK,
+        Err(DatabaseError::DatabaseExists(_)) => StatusCode::BAD_REQUEST,
+    }
 }
 
 #[derive(Deserialize)]
@@ -49,13 +82,10 @@ struct CreateDatabaseRequest {
 
 #[tokio::main]
 async fn main() {
-    let store = NoopStore {};
-    let app_state = AppState {
-        database_store: store,
-    };
-
+    let store = HashMapStore::new();
+    let app_state = AppState::new(store);
     let app = Router::new()
-        .route("/dbs", post(create_database::<NoopStore>))
+        .route("/dbs", post(create_database))
         .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
